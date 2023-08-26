20df57340649994e3e9d6ae6a61e0c18d9e84a6b
BluePanda <m.saratzidis@gmail.com>
2023-08-26 08:22:18 +0200

    everything is working with the API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import {
-  findProjects,
+  findQueryResponses,
+  updateQueryResponse,
 } from "./backEnd_api_func.js";
 
 
@@ -19,12 +20,70 @@ let repeatCalculateCVsummaryJobsNodesVar = setInterval(repeatCalculateCVsummaryJ
 
 async function repeatCalculateCVsummaryJobsNodesFunc() {
 
-  
-  console.log("hello = " )
+
+  let findQueryResponsesRes = await findQueryResponses();
 
 
+  for (let i = 0; i < findQueryResponsesRes.length; i++) { // for all the queries
+
+    let queryResponse = findQueryResponsesRes[i];
+
+    let responderChatID = await findUserChatID(queryResponse.responder); // what is the chatID of the future responder
+
+    let messageSendRes = await messageSend(queryResponse); // what is the message to send
+
+
+    console.log("messageSendRes = " , messageSendRes)
+    console.log("queryResponse._id = " , queryResponse._id)
+
+
+    
+
+    // --------------- update backend that message was sent ----------------
+    let res = await updateQueryResponse({
+      _id: queryResponse._id,
+      sentFlag: true,
+    })
+    // --------------- update backend that message was sent ----------------
+
+    sd9
+
+
+
+
+  }
   
+
+
   clearInterval(repeatCalculateCVsummaryJobsNodesVar);
-  
   repeatCalculateCVsummaryJobsNodesVar = setInterval(repeatCalculateCVsummaryJobsNodesFunc, speed_CalculateCVsummaryJobsNodes);
+}
+
+
+async function messageSend(queryResponse) {
+
+  let messageSend = queryResponse?.question?.content
+
+
+
+  return messageSend
+}
+
+
+async function findUserChatID(userInfo) {
+
+  let chatID = "23423"
+
+  // find the user if it is position
+  if (userInfo.positionID) {
+
+  }
+  // find the user if it is candidate
+  if (userInfo.userID) {
+
+  }
+
+
+
+  return chatID
 }
--- backEnd_api_func.js
+++ backEnd_api_func.js
@@ -1,21 +1,116 @@
 import apiClient from "./api/axios.js";
-// import apiClientCron from "./api/axiosCron.js";
-// import apiClientCron from "./api/axiosCron.js";
 
-export async function findProjects() {
+
+
+const queryResponseFields = `
+_id
+sender {
+  positionID
+  userID
+}
+responder {
+  positionID
+  userID
+}
+phase
+sentFlag
+question {
+  content
+}
+answer {
+  content
+}
+`;
+
+export async function findQueryResponses() {
+
+
   let res = await apiClient({
     data: {
-      query: `query{
-        findProjects(fields:{
-            # serverID: "996558082098339953"
-        }){
-            _id
-            title
-            
-        }
-        }`,
+      query: `query {
+                findQueryResponses(fields: {
+                  sentFlag: false
+                  phase: QUERY
+                }) {
+                ${queryResponseFields}
+              }
+            }`,
     },
+  }).catch((err) => {
+    console.log(err.response.data.errors);
   });
 
-  return res.data.data.findProjects;
+  if (res?.data?.data?.findQueryResponses) {
+    return res.data.data.findQueryResponses;
+  } else {
+    return [];
+  }
+}
+
+export async function updateQueryResponse(updateQuery) {
+
+
+  const textUpdate = updateQueryToText(updateQuery)
+
+  let res = await apiClient({
+    data: {
+      query: `mutation {
+                updateQueryResponse(fields: {
+                  ${textUpdate}
+                }) {
+                ${queryResponseFields}
+              }
+            }`,
+    },
+  }).catch((err) => {
+    console.log(err.response.data.errors);
+  });
+
+  if (res?.data?.data?.updateQueryResponse) {
+    return res.data.data.updateQueryResponse;
+  } else {
+    return [];
+  }
+}
+
+function updateQueryToText(updateQuery) {
+  let updateQueryText = "";
+  
+  if (updateQuery._id) {
+    updateQueryText += `_id: "${updateQuery._id}"`;
+  }
+
+  if (updateQuery.sentFlag) {
+    updateQueryText += `sentFlag: ${updateQuery.sentFlag}`;
+  }
+
+  if (updateQuery.phase) {
+    updateQueryText += `phase: ${updateQuery.phase}`;
+  }
+
+  if (updateQuery.question) {
+    updateQueryText += `question: "${updateQuery.question}"`;
+  }
+
+  if (updateQuery.answer) {
+    updateQueryText += `answer: "${updateQuery.answer}"`;
+  }
+
+  if (updateQuery.senderID) {
+    updateQueryText += `senderID: "${updateQuery.senderID}"`;
+  }
+
+  if (updateQuery.responderID) {
+    updateQueryText += `responderID: "${updateQuery.responderID}"`;
+  }
+
+  if (updateQuery.senderType) {
+    updateQueryText += `senderType: ${updateQuery.senderType}`;
+  }
+
+  if (updateQuery.responderType) {
+    updateQueryText += `responderType: ${updateQuery.responderType}`;
+  }
+
+  return updateQueryText;
 }
