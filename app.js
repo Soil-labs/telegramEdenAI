50a7c5369e09cb92d20b28ee2f3a7175bf4f0505
BluePanda <m.saratzidis@gmail.com>
2023-08-27 14:21:44 +0200

    feat/closeCircle

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import {
 
 import {
   checkIfFirstMessageTGConnection,
+  findChatIDforUser,
+  findIfMessageIsAResponse,
 } from "./telegram_func.js";
 
 
@@ -28,22 +30,33 @@ console.log("I am alive!");
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
 
+  if (!chatId) {
+    return
+  }
+
   console.log("msg = " , msg)
 
 
+  // --------------- check if first message TG connection ----------------
   let res = await checkIfFirstMessageTGConnection(msg)
+  if (res.done == true ){
+    let messageSentTelegram = ""
+    if (res.messageBack){
+      messageSentTelegram = res.messageBack
+    }
+    bot.sendMessage(chatId, messageSentTelegram);
 
-  console.log("res = " , res)
+    return 
+  } 
+  // --------------- check if first message TG connection ----------------
 
-  let messageSentTelegram = ""
-  if (res.done == true && res.messageBack){
-    messageSentTelegram = res.messageBack
-  } else {
-    messageSentTelegram = 'Received your message - ' + msg.text 
-  }
+  
+  findIfMessageIsAResponse(chatId,msg.text)
+  
+  bot.sendMessage(chatId, 'Thank you for the answer' );
+    
+  
 
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, messageSentTelegram);
 });
 // --------------- telegram bot ----------------
 
@@ -53,36 +66,105 @@ bot.on('message', async (msg) => {
 
 
 
-// // --------------- repeatCalculateCVsummaryJobsNodesFunc ----------------
-// const speed_CalculateCVsummaryJobsNodes = 10000;
-// let repeatCalculateCVsummaryJobsNodesVar = setInterval(repeatCalculateCVsummaryJobsNodesFunc, speed_CalculateCVsummaryJobsNodes);
-// // --------------- repeatCalculateCVsummaryJobsNodesFunc ----------------
+// --------------- Sent Message for un-send queries --------------
+const speed_CheckQueriesAndSent = 12000;
+let repeatCheckQueriesAndSentVar = setInterval(checkQueriesAndSentFunc, speed_CheckQueriesAndSent);
+// --------------- Sent Message for un-send queries --------------
+
+// --------------- Sent Message for un-send Responses --------------
+const speed_CheckResponsesAndSent = 8000;
+let repeatCheckResponsesAndSentVar = setInterval(checkResponsesAndSentFunc, speed_CheckResponsesAndSent);
+// --------------- Sent Message for un-send Responses --------------
+
+async function checkResponsesAndSentFunc() {
 
 
+  let findQueryResponsesRes = await findQueryResponses({
+    sentFlag: false,
+    phase: "RESPONDED",
+  });
+  // console.log("findQueryResponsesRes = " , findQueryResponsesRes)
 
-async function repeatCalculateCVsummaryJobsNodesFunc() {
+  for (let i = 0; i < findQueryResponsesRes.length; i++) { // for all the queries
+  // for (let i = 0; i < 1; i++) { // for all the queries
 
-
-  let findQueryResponsesRes = await findQueryResponses();
-
-
-  // for (let i = 0; i < findQueryResponsesRes.length; i++) { // for all the queries
-  for (let i = 0; i < 1; i++) { // for all the queries
 
     let queryResponse = findQueryResponsesRes[i];
 
-    let responderChatID = await findUserChatID(queryResponse.responder); // what is the chatID of the future responder
+    if (!queryResponse){
+      return 
+    }
 
-    let messageSendRes = await messageSend(queryResponse); // what is the message to send
+    // let messageSendRes = await messageSend(queryResponse); // what is the message to send
+
+    let messageSendRes = `You already have a response for your question:
+    Q: ${queryResponse?.question?.content}
+    A: ${queryResponse?.answer?.content}`
 
 
     console.log("messageSendRes = " , messageSendRes)
     console.log("queryResponse._id = " , queryResponse._id)
 
 
+    const chatID = await findChatIDforUser(queryResponse.sender)
+
+    console.log("chatID = " , chatID)
+
+
+    // sd1
+
+    if (chatID && messageSendRes){
+      // --------------- send message Telegram ----------------
+      bot.sendMessage(chatID, messageSendRes);
+      // --------------- send message Telegram ----------------
+    }
+
+
+    // --------------- update backend that message was sent ----------------
+    let res = await updateQueryResponse({
+      _id: queryResponse._id,
+      sentFlag: true,
+    })
+    // --------------- update backend that message was sent ----------------
+  }
+  
+
+  clearInterval(repeatCheckResponsesAndSentVar);
+  repeatCheckResponsesAndSentVar = setInterval(checkResponsesAndSentFunc, speed_CheckResponsesAndSent);
+}
+
+
+
+async function checkQueriesAndSentFunc() {
+
+
+  let findQueryResponsesRes = await findQueryResponses({
+    sentFlag: false,
+    phase: "QUERY",
+  });
+  // console.log("findQueryResponsesRes = " , findQueryResponsesRes)
+
+  for (let i = 0; i < findQueryResponsesRes.length; i++) { // for all the queries
+  // for (let i = 0; i < 1; i++) { // for all the queries
+
+    let queryResponse = findQueryResponsesRes[i];
+
+    // let messageSendRes = await messageSend(queryResponse); // what is the message to send
+    let messageSendRes = `Wow people are looking at your profile, you have a question:
+    ${queryResponse?.question?.content}`
+
+
+    console.log("messageSendRes = " , messageSendRes)
+    console.log("queryResponse._id = " , queryResponse._id)
+
+
+    const chatID = await findChatIDforUser(queryResponse.responder)
+
+    console.log("chatID = " , chatID)
+
 
     // --------------- send message Telegram ----------------
-    bot.sendMessage(responderChatID, messageSendRes);
+    bot.sendMessage(chatID, messageSendRes);
     // --------------- send message Telegram ----------------
 
 
@@ -93,19 +175,11 @@ async function repeatCalculateCVsummaryJobsNodesFunc() {
     })
     // --------------- update backend that message was sent ----------------
 
-
-
-    sd9
-
-
-
-
   }
   
 
-
-  clearInterval(repeatCalculateCVsummaryJobsNodesVar);
-  repeatCalculateCVsummaryJobsNodesVar = setInterval(repeatCalculateCVsummaryJobsNodesFunc, speed_CalculateCVsummaryJobsNodes);
+  clearInterval(repeatCheckQueriesAndSentVar);
+  repeatCheckQueriesAndSentVar = setInterval(checkQueriesAndSentFunc, speed_CheckQueriesAndSent);
 }
 
 
@@ -118,21 +192,3 @@ async function messageSend(queryResponse) {
   return messageSend
 }
 
-
-async function findUserChatID(userInfo) {
-
-  let chatID = "2125809502"
-
-  // find the user if it is position
-  if (userInfo.positionID) {
-
-  }
-  // find the user if it is candidate
-  if (userInfo.userID) {
-
-  }
-
-
-
-  return chatID
-}
